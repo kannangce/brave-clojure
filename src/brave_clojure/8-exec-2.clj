8587bd04ddef77e89798b5c420fac4493019f6f5
Kannan Ramamoorthy <kannan.r@imaginea.com>
2019-06-03 23:38:26 +0530

    custom or

---
+++ src/brave_clojure/8-exec-2.clj
@@ -0,0 +1,63 @@
+(defmacro my-or
+  "Equivalent of or"
+  ([] nil)
+  
+  ([expr]
+   `~expr)
+
+  ([expr & other-exprs]
+   `(let [val# (my-or ~expr)]
+      (if val# val# (my-or ~@other-exprs)))))
+      
+      
+(my-or
+  (do (println "(= 1 2)") (= 1 2))
+  (do (println "(= 2 3)") (= 2 3))
+  (do (println "(= 1 1)") (= 1 1)))
+
+;  (= 1 2)
+;  (= 2 3)
+;  (= 1 1)
+;  user=>
+;  true)  
+
+
+(my-or
+  (do (println "(= 1 1)") (= 1 1))
+  (do (println "(= 1 2)") (= 1 2))
+  (do (println "(= 2 3)") (= 2 3)))
+
+; (= 1 1)
+; user=>
+; true
+
+
+(my-or
+  (do (println "(= 1 2)") (= 1 2))
+  (do (println "(= 1 1)") (= 1 1))
+  (do (println "(= 2 3)") (= 2 3)))
+
+; (= 1 2)
+; (= 1 1)
+; user=>
+; true
+
+(my-or
+  (do (println "(= 1 2)") (= 1 2)))
+
+;(= 1 2)
+;user=>
+;false
+
+
+(my-or
+  (do (println "(= 1 1)") (= 1 1)))
+
+; (= 1 1)
+; user=>
+; true
+
+(my-or)
+
+; user=>
+; nil
